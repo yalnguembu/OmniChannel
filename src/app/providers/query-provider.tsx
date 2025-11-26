f2ec5b6c4024c2050af7d00c9d92d3879bfb48d3
yal.nguembu <yal.nguembu@graphicssystemsa.com>
2025-11-26 15:04:43 +0100

    enhance: disale query refresh on page focus change

--- src/app/providers/query-provider.tsx
+++ src/app/providers/query-provider.tsx
@@ -6,6 +6,9 @@ const queryClient = new QueryClient({
     queries: {
       staleTime: 1000 * 60 * 5,
       gcTime: 1000 * 60 * 10,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
     },
   },
 })
